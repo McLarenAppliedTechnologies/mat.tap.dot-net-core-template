4225a61a92ed866326934ca146fa2df31103c364
Sadeep Madurange <sadeepmadurange@gmail.com>
2018-07-09 17:25:00 +0800

    Added dbcontextfactory.

---
+++ TEST.API.Analytics.API/Factories/DbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TEST.API.Core.Factories;
+
+namespace TEST.API.Analytics.API.Factories
+{
+    public class DbContextFactory : IDbContextFactory
+    {
+        private readonly DbContextOptions<Model> options;
+
+        public DbContextFactory(IConfiguration configuration)
+        {
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<Model>();
+            dbContextOptionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            options = dbContextOptionsBuilder.Options;
+        }
+
+        public DbContext CreateNewDbContext()
+        {
+            return new Model(options);
+        }
+
+        public DbSet<TEntity> GetDbSet<TEntity>(DbContext dbContext) where TEntity : class
+        {
+            return dbContext.Set<TEntity>();
+        }
+    }
+}
